50bb0a7b31e54a24ea858a86e7304978a9355997
DanVi007 <danvitrinh@outlook.com>
2022-11-08 18:24:47 +0100

    nvim tree

--- lua/dan/nvim-tree.lua
+++ lua/dan/nvim-tree.lua
@@ -1,3 +1,5 @@
+-- look at this:
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
 -- disable netrw at the very start of your init.lua (strongly advised)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -5,3 +7,30 @@ vim.g.loaded_netrwPlugin = 1
 require("nvim-tree").setup()
 
 vim.cmd[[hi NvimTreeNormal guibg=NONE ctermbg=NONE]]
+
+-- examples for your init.lua
+
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    adaptive_size = true,
+    mappings = {
+      list = {
+        { key = "u", action = "dir_up" },
+      },
+    },
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
