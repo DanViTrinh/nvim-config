864861ba82b08d4c600af8e3f15daef6fb6b7d40
DanVi007 <danvitrinh@outlook.com>
2022-11-10 22:47:29 +0100

    supress error from lang server and clipboard

--- lua/dan/options.lua
+++ lua/dan/options.lua
@@ -8,21 +8,33 @@ vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 
 --vim.opt.signcolumn = "yes:1" -- https://stackoverflow.com/questions/18319284/vim-sign-column-toggle
                             -- https://www.reddit.com/r/neovim/comments/xq5dm3/nvimcmp_disable_syntax_check_column/
-
 vim.opt.signcolumn = "yes"
+
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.showtabline =2 
+vim.opt.showtabline =2
 
-vim.opt.mouse ="a" 
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.mouse ="a"
 
 vim.opt.colorcolumn = "80"
-
 
 vim.wo.fillchars = 'eob: ' -- to remove tilde 
 
 -- uncomment for pwsh
 vim.opt.shell ="pwsh"
 
-
+-- https://www.reddit.com/r/neovim/comments/q4lzki/client_1_quit_with_exit_code_1_and_signal_0_when/
+-- suppress error messages from lang servers
+vim.notify = function(msg, log_level, _opts)
+    if msg:match("exit code") then
+        return
+    end
+    if log_level == vim.log.levels.ERROR then
+        vim.api.nvim_err_writeln(msg)
+    else
+        vim.api.nvim_echo({{msg}}, true, {})
+    end
+end
