39be9ca2144c429717aaa14a27523f1b747467c4
DanVi007 <danvitrinh@outlook.com>
2022-12-01 15:24:32 +0100

    added close buffer

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -31,7 +31,11 @@ keymap("n", "<Leader>tc",":lua require('cmp').setup {enabled = false}<CR>",opts)
 -- save
 keymap("n", "<Leader>w",":w<CR>",opts)
 -- quit 
-keymap("n", "<Leader>q","<cmd>q<CR>",opts)
+keymap("n", "<Leader>q",":q<CR>",opts)
+
+
+keymap("n", "<C-q>","<cmd>bdelete<cr>",opts)
+
 
 --tab and windows 
 keymap("n", "<A-h>",":bp<CR>",opts)
