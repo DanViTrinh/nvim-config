b8a4aec340c48795b24cb49bbe99c2a627a4b951
DanVi007 <danvt@stud.ntnu.no>
2023-03-15 16:07:03 +0100

    added mac keybindings

--- lua/core/keymaps/normal.lua
+++ lua/core/keymaps/normal.lua
@@ -10,21 +10,27 @@ keymap("n", "<Leader>q",":q<CR>",opts)
 keymap("n", "<C-q>","<cmd>bdelete<cr>",opts)
 
 --tab and windows 
--- keymap("n", "˛",":bp<CR>",opts) -- for mac  
--- keymap("n", "ﬁ",":bn<CR>",opts) -- for mac
-keymap("n", "<A-h>",":BufferPrevious<CR>",opts)
-keymap("n", "<A-l>",":BufferNext<CR>",opts)
 keymap('n', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
 keymap('n', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
 keymap('n', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
 keymap('n', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
 
+ 
+if vim.fn.has('macunix') then
+  keymap("n", "˛",":bp<CR>",opts) -- for mac  
+  keymap("n", "ﬁ",":bn<CR>",opts) -- for mac
+  keymap("n", "ª","<C-y>",opts) -- faster movement
+  keymap("n", "√","<C-e>",opts) -- faster movement
+else
+  keymap("n", "<A-h>",":BufferPrevious<CR>",opts)
+  keymap("n", "<A-l>",":BufferNext<CR>",opts)
+  keymap("n", "<A-k>","<C-y>",opts) -- faster movement
+  keymap("n", "<A-j>","<C-e>",opts) -- faster movement
+end
+
 
 keymap("n", "J","5j",opts) -- faster movement
 keymap("n", "K","5k",opts) -- faster movement 
-keymap("n", "<A-k>","<C-y>",opts) -- faster movement
-keymap("n", "<A-j>","<C-e>",opts) -- faster movement
-
 -- keymap("n", "√","J",opts) -- replace J -- for mac
 
 keymap("n", "<Leader>j","J",opts) -- replace J
