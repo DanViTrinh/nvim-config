d66a3b99263706707a0960ccc100e62727332101
DanVi007 <danvitrinh@outlook.com>
2023-03-17 19:12:22 +0100

    vim iluminate

--- lua/codeRelated/vim-illuminate.lua
+++ lua/codeRelated/vim-illuminate.lua
@@ -1,8 +1,9 @@
+local cursorLineColor = '#3b3f52'
 vim.cmd(':hi clear IlluminatedWordText')
-vim.cmd(':hi def IlluminatedWordText guibg=#3b3f52')
+vim.cmd(':hi def IlluminatedWordText guibg=' .. cursorLineColor)
 
 vim.cmd(':hi clear IlluminatedWordWrite')
-vim.cmd(':hi def IlluminatedWordWrite guibg=#3b3f52')
+vim.cmd(':hi def IlluminatedWordWrite guibg=' .. cursorLineColor)
 
 vim.cmd(':hi clear IlluminatedWordRead')
-vim.cmd(':hi def IlluminatedWordRead guibg=#3b3f52')
+vim.cmd(':hi def IlluminatedWordRead guibg=' .. cursorLineColor)
